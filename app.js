7a3b37b9347ad20cd4b3f9806bb5c0ba4c18fde0
secdice <xwaxcwh6zhdkm3mc32v9ts@Safe-mail.net>
2015-07-27 18:13:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var config = {
   app_name: 'SecDice',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
-  recaptcha_sitekey: '6LfI_QUTAAAAACrjjuzmLw0Cjx9uABxb8uguLbph',  // <----- EDIT ME!
+  recaptcha_sitekey: '6LckZQoTAAAAAMrgEossoTt4nJZTevXTtdfrIQtQ',  // <----- EDIT ME!
   redirect_uri: 'https://secdice.github.io',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
